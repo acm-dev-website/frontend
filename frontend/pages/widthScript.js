5a8b808012345da296857539fd3661f77e8b0190
Zen L <zenlambertus@gmail.com>
2024-04-12 16:03:29 -0400

    more updates

--- frontend/pages/widthScript.js
+++ frontend/pages/widthScript.js
@@ -2,16 +2,18 @@ function updateScreenWidth() {
     // Get the width of the screen
     let screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-    if (screenWidth > 1200)
-        screenWidth = 1200;
+    //if (screenWidth > 1200)
+      //  screenWidth = 1200;
 
     // Set the screen width as a CSS variable
     document.documentElement.style.setProperty('--screen-width', `${screenWidth}px`);
     console.log("Width is %d", screenWidth);
 
-    const scale = screenWidth/1200;
-    document.documentElement.style.setProperty('--screenScale', `${scale}`);
-    console.log("Scale is %f", scale);
+    const screenScale = screenWidth/1200;
+    document.documentElement.style.setProperty('--screenScale', `${screenScale}`);
+    console.log("Scale is %f", screenScale);
+
+    //const marginScale = screenScale;
 }
 
 // Call the function initially to set the CSS variable
